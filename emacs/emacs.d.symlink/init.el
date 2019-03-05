64e0f2cfacdb0f73d08e557154a5e346bc63c479
Bowen Masco <bowen@codingfoo.com>
2019-03-05 09:37:37 -0800

    Add template for daily schedule

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -24,6 +24,41 @@
 (set-frame-font "Monaco 18" nil t)
 
 
+(setq org-agenda-files '("~/b0m00jo/"))
+
+'(progn
+    (add-to-list 'org-structure-template-alist
+'("t" "* Daily-?
+|  Time | Plan | Revised | Notes |
+|-------+------+---------+-------|      
+|  6:00 |      |         |       |
+|  6:30 |      |         |       |
+|  7:00 |      |         |       |
+|  7:30 |      |         |       |
+|  8:00 |      |         |       |
+|  8:30 |      |         |       |
+|  9:00 |      |         |       |
+|  9:30 |      |         |       |
+| 10:00 |      |         |       |
+| 10:30 |      |         |       |
+| 11:00 |      |         |       |
+| 11:30 |      |         |       |
+| 12:00 |      |         |       |
+| 12:30 |      |         |       |
+|  1:00 |      |         |       |
+|  1:30 |      |         |       |
+|  2:00 |      |         |       |
+|  2:30 |      |         |       |
+|  3:00 |      |         |       |
+|  3:30 |      |         |       |
+|  4:00 |      |         |       |
+|  4:30 |      |         |       |
+|  5:00 |      |         |       |
+|  5:30 |      |         |       |
+|  6:00 |      |         |       |
+|--------------------------------|")))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
